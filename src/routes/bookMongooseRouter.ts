8e515d7da46c9c5ec673c725c1d18c0f267120c6
DmitriiBank <dm.skakov@gmail.com>
2025-08-10 13:23:08 +0300

    Added mongoose

--- src/routes/bookMongooseRouter.ts
+++ src/routes/bookMongooseRouter.ts
@@ -8,61 +8,36 @@ import {
     returnBook
 } from "../services/libMongooseService.js"
 import {BookGenres} from "../model/Book.js";
+import asyncHandler from "express-async-handler";
 
 export const bookRouter = express.Router()
 
-bookRouter.get('/', async (req, res) => {
+bookRouter.get('/', asyncHandler(async (req, res) => {
     const books = await getAllBooks()
     res.json(books)
-});
+}));
 
-bookRouter.post('/', async (req, res) => {
-    try {
+bookRouter.post('/', asyncHandler(async (req, res) => {
         const book = await addBook(req.body)
         res.status(201).json(book)
-    } catch (e) {
-        const error = e as Error;
-        res.status(400).send(error.message)
-    }
-})
+}))
+bookRouter.get('/genre/:genre', asyncHandler(async (req, res) => {
+    const genre = req.params.genre as BookGenres;
+    const booksByGenre = await getBooksByGenre(genre);
+    res.status(201).json(booksByGenre);
+}));
 
-bookRouter.get('/genre/:genre', async (req, res) => {
-    try {
-        const genre = req.params.genre as BookGenres;
-        const booksByGenre = await getBooksByGenre(genre);
-        res.status(201).json(booksByGenre)
-    } catch (e) {
-        const error = e as Error;
-        res.status(400).send(error.message)
-    }
-})
-bookRouter.delete('/', async (req, res) => {
-    try {
-        const removedBook = await removeBook(req.body.id);
-        res.status(201).json(removedBook)
-    } catch (e) {
-        const error = e as Error;
-        res.status(400).send(error.message)
-    }
-})
+bookRouter.delete('/', asyncHandler(async (req, res) => {
+    const removedBook = await removeBook(req.body.id);
+    res.status(201).json(removedBook);
+}));
 
-bookRouter.post('/on_stock', async (req, res) => {
-    try {
-        const pickedUpBook = await pickUpBook(req.body.id, req.body.reader);
-        res.status(201).json(pickedUpBook)
-    } catch (e) {
-        const error = e as Error;
-        res.status(400).send(error.message)
-    }
-})
+bookRouter.post('/on_stock', asyncHandler(async (req, res) => {
+    const pickedUpBook = await pickUpBook(req.body.id, req.body.reader);
+    res.status(201).json(pickedUpBook);
+}));
 
-
-bookRouter.post('/on_hand', async (req, res) => {
-    try {
-        const returnedBook = await returnBook(req.body.id);
-        res.status(201).json(returnedBook)
-    } catch (e) {
-        const error = e as Error;
-        res.status(400).send(error.message)
-    }
-})
+bookRouter.post('/on_hand', asyncHandler(async (req, res) => {
+    const returnedBook = await returnBook(req.body.id);
+    res.status(201).json(returnedBook);
+}));
