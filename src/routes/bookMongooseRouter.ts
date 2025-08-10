398ddd358e3b462695ca45314321ef1421b63175
DmitriiBank <dm.skakov@gmail.com>
2025-08-10 14:51:53 +0300

    Added mongoose

--- src/routes/bookMongooseRouter.ts
+++ src/routes/bookMongooseRouter.ts
@@ -1,43 +1,32 @@
 import express from "express";
-import {
-    getAllBooks,
-    addBook,
-    getBooksByGenre,
-    removeBook,
-    pickUpBook,
-    returnBook
-} from "../services/libMongooseService.js"
-import {BookGenres} from "../model/Book.js";
+import {BookController} from "../controllers/BookMongooseController.js";
 import asyncHandler from "express-async-handler";
 
 export const bookRouter = express.Router()
 
+const controller = new BookController();
+
 bookRouter.get('/', asyncHandler(async (req, res) => {
-    const books = await getAllBooks()
-    res.json(books)
+    await controller.getAllBooks(req, res);
 }));
 
 bookRouter.post('/', asyncHandler(async (req, res) => {
-        const book = await addBook(req.body)
-        res.status(201).json(book)
-}))
+    await controller.addBook(req, res);
+}));
+
 bookRouter.get('/genre/:genre', asyncHandler(async (req, res) => {
-    const genre = req.params.genre as BookGenres;
-    const booksByGenre = await getBooksByGenre(genre);
-    res.status(201).json(booksByGenre);
+    await controller.getBooksByGenre(req, res);
 }));
 
 bookRouter.delete('/', asyncHandler(async (req, res) => {
-    const removedBook = await removeBook(req.body.id);
-    res.status(201).json(removedBook);
+    await controller.removeBook(req, res);
 }));
 
-bookRouter.post('/on_stock', asyncHandler(async (req, res) => {
-    const pickedUpBook = await pickUpBook(req.body.id, req.body.reader);
-    res.status(201).json(pickedUpBook);
+bookRouter.post('/on_stock',  asyncHandler(async (req, res) => {
+    await controller.pickUpBook(req, res);
 }));
 
 bookRouter.post('/on_hand', asyncHandler(async (req, res) => {
-    const returnedBook = await returnBook(req.body.id);
-    res.status(201).json(returnedBook);
+    await controller.returnBook(req, res);
 }));
+
