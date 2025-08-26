9104e95f7007ab96caaf633afcbbf7757a3f9d0c
DmitriiBank <dm.skakov@gmail.com>
2025-08-26 13:45:09 +0300

    added method to accountService.ts

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,8 +1,11 @@
 import {Reader} from "../model/Reader.js";
+import {Roles} from "../utils/libTypes.js";
 
 export interface AccountService {
     addAccount: (reader: Reader) => Promise<void>;
     getAccount: (id: string) => Promise<Reader>;
     removeAccount: (id: string) => Promise<Reader>;
     changePassword: (id: string, newPassword: string) => Promise<void>;
+    changeReaderData: (id: string, newUserName: string, newEmail: string, newBirthdate: Date) => Promise<void>;
+    changeReaderRole: (id: string, newRole: Roles) => Promise<void>
 }
